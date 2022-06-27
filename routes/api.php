0f00fec933e003589fef210184842fd8fcfde02b
Nikola <5dzoni5@gmail.com>
2022-06-27 18:46:41 +0400

    Finished Logout Route

--- routes/api.php
+++ routes/api.php
@@ -3,15 +3,33 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+// Models
 use App\Models\Product;
+
+// Controllers
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\AuthController;
 
+// Public Routes
 
-Route::resource('/products', ProductController::class);
+Route::post('/register', [AuthController::class, 'register']);
+
+Route::get('/products', [ProductController::class, 'index']);
+
+Route::get('/products/{id}', [ProductController::class, 'show']);
 
 Route::get('/products/search/{name}', [ProductController::class, 'search']);
 
+// Protected Routes
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => ['auth:sanctum']], function () {
+
+    Route::post('/products', [ProductController::class, 'store']);
+
+    Route::put('/products/{id}', [ProductController::class, 'update']);
+
+    Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+
+    Route::post('/logout', [AuthController::class, 'logout']);
+
 });
